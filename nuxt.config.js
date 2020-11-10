c95d047a3f43a584655cb381c6b61425d95eea55
z3to <6gqrbj@posteo.org>
2020-11-10 21:14:11 +0100

    Fix routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,12 @@
+const { getHead } = require('library/src/assets/js/head.js')
 
 export default {
   mode: 'spa',
-  head: {
+  head: getHead({
     title: 'Co-producing robust adaptation pathways to address adaptation and mitigation to climate change',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'In this module, we illustrate how co-production techniques can be applied in combination with socioeconomic and impact scenarios to develop adaptation pathways for a climate robust and CO2-neutral landscape. We demonstrate this process with the case study of the Dutch Overijsselse Vecht.' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+    description: 'In this module, we illustrate how co-production techniques can be applied in combination with socioeconomic and impact scenarios to develop adaptation pathways for a climate robust and CO2-neutral landscape. We demonstrate this process with the case study of the Dutch Overijsselse Vecht.',
+    image: 'https://climatescenarios.org/share/og-dutch.jpg'
+  }),
   loading: { color: '#fff' },
   css: [
     '@/assets/style/base.scss'
@@ -27,5 +22,8 @@ export default {
   build: {
     extend (config, ctx) {
     }
+  },
+  generate: {
+    crawler: false
   }
 }
