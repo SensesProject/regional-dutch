657589ed9bc7cbcc70146107af76b56afd2bc4dc
z3to <6gqrbj@posteo.org>
2020-11-10 21:14:21 +0100

    Add Senses Menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
+    <SensesMenu />
     <nuxt />
   </div>
 </template>
 
+<script>
+import SensesMenu from 'library/src/components/SensesMenu.vue'
+
+export default {
+  components: {
+    SensesMenu
+  }
+}
+</script>
 <style>
 
 </style>
