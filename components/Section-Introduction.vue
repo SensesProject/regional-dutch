02fb874d356d02043fa2d89be23e076208f4f045
z3to <6gqrbj@posteo.org>
2020-12-02 12:51:16 +0100

    Caption added

--- components/Section-Introduction.vue
+++ components/Section-Introduction.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <section id="introduction" class="step step--wider">
       <header>
+        <span class="caption">Dutch Case Study</span>
         <h1>Co-producing robust adaptation pathways to address adaptation and mitigation to climate change</h1>
       </header>
       <p>The Dutch case study area, the Overijsselse Vecht, is an intensely managed area and has a long history of participatory management and cooperation between public and private sector stakeholders and scientists. The fields of interest in the Overijsselse Vecht include water management, (sustainable) agricultural development and climate change adaptation. For these reasons, the Dutch case study is an example of an <a href="https://climatescenarios.org/co-production/#dutch" target="_blank">information-rich context</a>. We link the local, bottom-up and empirical knowledge with top-down scientific input translated from global socioeconomic and climate impact scenarios, developed from the <a href="https://climatescenarios.org/primer/socioeconomic-development" target="_blank">Shared Socioeconomic Pathways</a> using co-production techniques with stakeholders. The aim is to develop pathways for a climate robust and CO₂-neutral Overijsselse Vecht, while linking climate adaptation and mitigation. In this page, adaptation scenarios are referred to as “pathways” because we design a “path” in coherent steps in time towards a sustainability goal that we call “vision”.</p>
